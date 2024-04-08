475f363209fe2f0e9769418e120084c104c8f7f8
Sungtae Kim <tsk0204@gmail.com>
2024-04-09 01:20:36 +0900

    "Go to Main" button added

--- TexTris/src/main/java/org/Stech/SE5/View/RecordView.java
+++ TexTris/src/main/java/org/Stech/SE5/View/RecordView.java
@@ -8,9 +8,10 @@ import org.Stech.SE5.Controller.RecordController;
 
 public class RecordView extends JFrame {
     private static RecordController recordController;
+    private JButton goToMainBtn;
 
     public RecordView(final RecordController controller) {
-        super("TETRIS");
+        super("Record");
         recordController = controller;
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -24,6 +25,16 @@ public class RecordView extends JFrame {
         recordPanel.setLayout(new BorderLayout());
         recordPanel.setVisible(true);
         recordPanel.setLayout(null);
+
+        goToMainBtn = new JButton("Go To Main");
+        goToMainBtn.setBounds(100, 500, 200, 40);
+        goToMainBtn.setBorderPainted(false); // 경계선이 보이지 않도록 설정
+        goToMainBtn.setContentAreaFilled(true); //배경색이 보이도록 설정
+        goToMainBtn.setBackground(Color.LIGHT_GRAY); //배경색은 밝은 회색
+        goToMainBtn.setForeground(Color.WHITE); // 버튼 텍스트 색상을 흰색으로 설정합니다.
+        goToMainBtn.setFont(new Font("Arial", Font.CENTER_BASELINE, 20)); //폰트 및 크기 설정
+
+        recordPanel.add(goToMainBtn);
 
         this.setContentPane(recordPanel);
         setFocusable(true);
