a7de0f54c775270c3c3ffb9baeb8676de98a5c22
Sungtae Kim <tsk0204@gmail.com>
2024-04-09 21:25:17 +0900

    Removed Interface Controller
    Controller Interface is merged into RecordController

--- TexTris/src/main/java/org/Stech/SE5/Controller/RecordController.java
+++ TexTris/src/main/java/org/Stech/SE5/Controller/RecordController.java
@@ -1,13 +1,12 @@
 package org.Stech.SE5.Controller;
 
-import org.Stech.SE5.Data.Controller;
 import org.Stech.SE5.Data.Record;
 import org.Stech.SE5.Model.RecordModel;
 import org.Stech.SE5.View.RecordView;
 
 import java.util.ArrayList;
 
-public class RecordController implements Controller {
+public class RecordController {
     private RecordView recordView;
     private final int VIEW_WIDTH = 400;
     private final int VIEW_HEIGHT = 600;
@@ -16,12 +15,10 @@ public class RecordController implements Controller {
         initController();
     }
 
-    @Override
     public void initController() {
         recordView = new RecordView(this);
     }
 
-    @Override
     public void setVisible(boolean visible) {
         if (visible) {
             recordView.setSize(VIEW_WIDTH, VIEW_HEIGHT);
--- TexTris/src/main/java/org/Stech/SE5/Data/Controller.java
+++
@@ -1,6 +0,0 @@
-package org.Stech.SE5.Data;
-
-public interface Controller {
-    void initController();
-    void setVisible(boolean visible);
-}
