7fa403e03a13108477929999de56cf00e65e7e7e
Sungtae Kim <128397778+Sungtae124@users.noreply.github.com>
2024-04-25 20:42:08 +0900

    Revert "Record & GameEnd merge try"

--- TexTris/src/main/java/org/Stech/SE5/Main.java
+++ TexTris/src/main/java/org/Stech/SE5/Main.java
@@ -1,18 +1,7 @@
 package org.Stech.SE5;
 
-import org.Stech.SE5.Controller.RecordController;
-import org.Stech.SE5.Model.RecordModel;
-import org.Stech.SE5.View.GameEndView;
-
 public class Main {
     public static void main(String[] args) {
-        //RecordModel.clearRecord();
-
-        GameEndView gameEnd = new GameEndView(100, 0,0);
-        gameEnd.setVisible(true);
-
-        //record.setVisible(true);
-
-
+        System.out.println("Hello world!");
     }
 }
